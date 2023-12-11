01a24a1e6d53431dc92d4efc42c8255155327b15
pavankalyanmarupaka <pavananshumun@gmail.com>
2023-12-11 09:51:53 +0000

    solution

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import {v4 as uuidV4} from 'uuid'
 
 import TodoItem from '../TodoItem'
 
@@ -40,28 +41,69 @@ const initialTodosList = [
 ]
 
 class SimpleTodos extends Component {
-  state = {todoList: initialTodosList}
+  state = {
+    todosList: initialTodosList,
+    titleInput: '',
+  }
+
+  renderTodoInputField = () => {
+    const {titleInput} = this.state
+
+    const onChangeHandler = event => {
+      this.setState({titleInput: event.target.value})
+    }
+
+    const onAddTodo = () => {
+      if (titleInput === '') return
+
+      this.setState(prevState => ({
+        todosList: [...prevState.todosList, {id: uuidV4(), title: titleInput}],
+        titleInput: '',
+      }))
+    }
+
+    return (
+      <div className="title-input-container">
+        <input value={titleInput} onChange={onChangeHandler} />
+        <button className="add-btn" type="button" onClick={onAddTodo}>
+          Add
+        </button>
+      </div>
+    )
+  }
 
   deleteTodo = id => {
-    const {todoList} = this.state
-    const updateTodoList = todoList.filter(eachTodo => eachTodo.id !== id)
+    const {todosList} = this.state
+    const updatedTodosList = todosList.filter(eachTodo => eachTodo.id !== id)
 
-    this.setState({todoList: updateTodoList})
+    this.setState({
+      todosList: updatedTodosList,
+    })
+  }
+
+  saveTodo = task => {
+    this.setState(prevState => ({
+      todosList: prevState.todosList.map(item =>
+        item.id === task.id ? task : item,
+      ),
+    }))
   }
 
   render() {
-    const {todoList} = this.state
+    const {todosList} = this.state
 
     return (
       <div className="app-container">
-        <div className="todo-container">
+        <div className="simple-todos-container">
           <h1 className="heading">Simple Todos</h1>
+          {this.renderTodoInputField()}
           <ul className="todos-list">
-            {todoList.map(eachTodo => (
+            {todosList.map(eachTodo => (
               <TodoItem
                 key={eachTodo.id}
                 todoDetails={eachTodo}
                 deleteTodo={this.deleteTodo}
+                saveTodo={this.saveTodo}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,18 +1,53 @@
 // Write your code here
+import {useState} from 'react'
+
 import './index.css'
 
 const TodoItem = props => {
-  const {todoDetails, deleteTodo} = props
-  const {title, id} = todoDetails
+  const {todoDetails, deleteTodo, saveTodo} = props
+  const {id, title} = todoDetails
+
+  const [isSaved, setIsSaved] = useState(true)
+  const [newTitle, setNewTitle] = useState(title)
 
   const onDeleteTodo = () => {
     deleteTodo(id)
   }
 
+  const onSaveTodo = () => {
+    setIsSaved(true)
+    saveTodo({id, title: newTitle})
+  }
+
+  const onEditClick = () => setIsSaved(false)
+
+  const onChangeHandler = event => setNewTitle(event.target.value)
+
   return (
-    <li className="list-container">
-      <p className="title">{title}</p>
-      <button className="button" type="button" onClick={onDeleteTodo}>
+    <li className="todo-item">
+      {isSaved ? (
+        <p className="title">{title}</p>
+      ) : (
+        <input className="title" value={newTitle} onChange={onChangeHandler} />
+      )}
+      {isSaved ? (
+        <button
+          type="button"
+          className="delete-btn edit-btn"
+          onClick={onEditClick}
+        >
+          Edit
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="delete-btn save-btn"
+          onClick={onSaveTodo}
+        >
+          Save
+        </button>
+      )}
+      <button type="button" className="delete-btn" onClick={onDeleteTodo}>
         Delete
       </button>
     </li>
